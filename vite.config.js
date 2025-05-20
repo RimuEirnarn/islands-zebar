62b11e8dc71a14ba399a460b24896434087ca6ce
Rimu Eirnarn <Rimuru720@proton.me>
2025-05-21 00:28:39 +0700

    Refactor Vite configuration to support mode-based settings and update dev script for build mode

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,28 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'config.json',
-          dest: '', // root of dist/
-          rename: 'islands.zebar.json'
-        }
-      ]
-    })
-  ],
-  build: {
-    outDir: '../island-dist', // This is the default but explicit now
-    emptyOutDir: true, // Clean `dist/` before building
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      react(),
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'config.json',
+            dest: '', // root of dist/
+            rename: 'islands.zebar.json'
+          }
+        ]
+      })
+    ],
+    build: {
+      outDir: '../island-dist', // This is the default but explicit now
+      emptyOutDir: true, // Clean `dist/` before building
+      sourcemap: mode !== 'production',
+      minify: mode === 'production'
+    },
+    define: {
+      __DEV__: mode !== 'production'
+    }
+  }
 });

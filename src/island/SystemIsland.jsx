7c72236cba976f3b5ce685e4754cf06b8b8d50e8
Rimu Eirnarn <Rimuru720@proton.me>
2025-05-21 13:34:10 +0700

    a

--- src/island/SystemIsland.jsx
+++ src/island/SystemIsland.jsx
@@ -4,25 +4,10 @@ import Memory from "../component/system/Memory";
 import Network from "../component/system/Network";
 import Weather from "../component/system/Weather";
 
-const OUTPUT_DEFAULT = {
-  disk: {
-    disks: [{
-      mountPoint: "A:\\",
-      totalSpace: {
-        siValue: NaN,
-        siUnit: "kB"
-      },
-      availableSpace: {
-        siValue: NaN,
-        siUnit: 'kB'
-      },
-      drive: "???"
-    }]
-  }
-}
+const DEFAULT = {}
 
 export default function SystemIsland(proc) {
-  const output = proc.network ? proc : OUTPUT_DEFAULT
+  const output = proc.network ? proc : DEFAULT
 
   const { network, memory, cpu, battery, weather } = output
   return <div className="right">
